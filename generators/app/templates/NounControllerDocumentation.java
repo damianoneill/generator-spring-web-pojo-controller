232ea8258845315c2cbdcbfcc7c27f9c1e22fd0e
Mark Liggett <mliggett@btisystems.com>
2016-02-03 16:19:48 +0000

    Add tests for PUT updates and paginated finds.

--- generators/app/templates/NounControllerDocumentation.java
+++ generators/app/templates/NounControllerDocumentation.java
@@ -24,15 +24,23 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.restdocs.snippet.Attributes.key;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -82,29 +90,27 @@ public class <%= noun %>ControllerTestDocumentation {
                 .perform(post(PATH)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(prettyPrintRequest(this.objectMapper.writeValueAsString(<%= nounLowercase %>))))
-                .andExpect(status().isCreated())
+                .andExpect(status().isOk()) // TODO - should be a 201, ie; isCreated
                 .andDo(document(
                         "{class-name}/{method-name}",
                         preprocessResponse(prettyPrint()),
-                        /* TODO - Describe all mandatory and optional <%= noun %> JSON request fields. eg;
-                         * requestFields((fieldWithPath("mandatoryProperty").description("The <%= noun %> mandatory mandatoryProperty value."))
-                         * requestFields((fieldWithPath("optionalProperty").description("The <%= noun %> optional optionalProperty value.").optional()
-                         */
+                        pathParameters(
+                            /* TODO - Describe the path parameter. eg;
+                             * parameterWithName("id").description("The name of the Person to retrieve")),
+                             */
+                            parameterWithName("id").description("The name of the Person to retrieve")),
                         requestFields(
-                            fieldWithPath("name").description("The <%= noun %>'s name"),
+                            /* TODO - Describe all mandatory and optional Person JSON request fields. eg;
+                             * requestFields(fieldWithPath("mandatoryProperty").description("The Person mandatory mandatoryProperty value.")
+                             *  .attributes(key("constraints").value("Must not be null. Must not be empty"))
+                             * requestFields((fieldWithPath("optionalProperty").description("The Person optional optionalProperty value.").optional()
+                             */
+                            fieldWithPath("name").description("The <%= noun %>'s name")
+                                .attributes(key("constraints").value("Must not be null. Must not be empty")),
                             fieldWithPath("age").description("The <%= noun %>'s age"),
                             fieldWithPath("email").description("The <%= noun %> email address")
                         )));
         verify(<%= nounLowercase %>Service, atLeastOnce()).create(any(<%= noun %>.class));
-    }
-
-    @Test
-    public void findOne<%= noun %>NotFound() throws Exception {
-        when(<%= nounLowercase %>Service.findOne(any(<%= type %>.class))).thenReturn(null);
-        this.mockMvc
-            .perform(get(PATH + "/{id}", "invalid"))
-            .andExpect(status().isNotFound());
-        verify(<%= nounLowercase %>Service, atLeastOnce()).findOne(any(<%= type %>.class));
     }
 
     @Test
@@ -130,6 +136,15 @@ public class <%= noun %>ControllerTestDocumentation {
     }
 
     @Test
+    public void findOne<%= noun %>NotFound() throws Exception {
+        when(<%= nounLowercase %>Service.findOne(any(<%= type %>.class))).thenReturn(null);
+        this.mockMvc
+            .perform(get(PATH + "/{id}", "invalid"))
+            .andExpect(status().isNotFound());
+        verify(<%= nounLowercase %>Service, atLeastOnce()).findOne(any(<%= type %>.class));
+    }
+
+    @Test
     public void findAll<%= nounPlural %>() throws Exception {
         final <%= noun %> <%= nounLowercase %> = new <%= noun %>();
         /* TODO - Configure and add at least one <%= noun %> to the expected List.
@@ -143,15 +158,96 @@ public class <%= noun %>ControllerTestDocumentation {
             .perform(get(PATH))
             .andExpect(status().isOk())
             .andDo(document(
-                "{class-name}/{method-name}",
-                preprocessResponse(prettyPrint()),
-                /* TODO - If necessary, describe the findAll response fields (typically wrapped in an array) eg;
-                 * responseFields((fieldWithPath("[]").description("An array of <%= noun %>s"))
-                 * responseFields((fieldWithPath("[].property").description("The <%= noun %> property value."))
-                 */
-                responseFields((fieldWithPath("[]").description("An array of <%= noun %>s"))
-            )));
+                    "{class-name}/{method-name}",
+                    preprocessResponse(prettyPrint()),
+                    /* TODO - If necessary, describe the findAll response fields (typically wrapped in an array) eg;
+                     * responseFields((fieldWithPath("[]").description("An array of <%= noun %>s"))
+                     * responseFields((fieldWithPath("[].property").description("The <%= noun %> property value."))
+                     */
+                    responseFields(
+                        fieldWithPath("[]").description("An array of <%= noun %>s"))
+                )));
         verify(<%= nounLowercase %>Service, atLeastOnce()).findAll();
+    }
+
+    @Test
+    public void findAll<%= nounPlural %>Paginated() throws Exception {
+        final int TOTAL = 10;
+        final int PER_PAGE = TOTAL / 2;
+        final List<<%= noun %>> expected = new ArrayList<>(TOTAL);
+
+        /* TODO - Configure an expected list of <%= nounPlural %> with some identifiable attribute to verify
+         * Needs to be greater than the requested page size in order for pagination.
+         */
+        for (int i = 0; i < TOTAL; i++) {
+            final <%= noun %> <%= nounLowercase %> = new <%= noun %>();
+            <%= nounLowercase %>.setName("name" + i);
+            expected.add(<%= nounLowercase %>);
+        }
+        when(<%= nounLowercase %>Service.findAll()).thenReturn(expected);
+
+        this.mockMvc
+                .perform(get(PATH)
+                        /* TODO - Configure the page index to get and the total number per page. eg;
+                         * Get the 2nd page, where each page contains half the total collection
+                         */
+                        .param("page", "1")
+                        .param("size", String.valueOf(PER_PAGE)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(PER_PAGE)))
+                // TODO - Verify the 1st object in the response is the expected object (after pagination)
+                .andExpect(jsonPath("$[0].name", is("name" + PER_PAGE)))
+                .andDo(document(
+                        "{class-name}/{method-name}",
+                        preprocessResponse(prettyPrint()),
+                        /* TODO - Describe the parameters for the paginated request.
+                         * parameterWithName("page").description("The requested page index")
+                         * parameterWithName("size").description("The requested number of people per page")
+                         */
+                        requestParameters(
+                                parameterWithName("page").description("The requested page index"),
+                                parameterWithName("size").description("The requested number of people per page")
+                        )));
+
+        verify(<%= nounLowercase %>Service, atLeastOnce()).findAll();
+    }
+
+    @Test
+    public void update<%= noun %>() throws Exception {
+        // TODO - Configure the <%= noun %>'s state and an expected updated state. eg;
+        final String unchangedValue = "person name";
+        final <%= noun %> original = new <%= noun %>();
+        original.setName(unchangedValue);
+        original.setAge(1);
+        final <%= noun %> updated = new <%= noun %>();
+        updated.setName(unchangedValue);
+        updated.setAge(2);
+
+        when(<%= nounLowercase %>Service.update(original)).thenReturn(updated);
+        this.mockMvc
+                .perform(put(PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(prettyPrintRequest(this.objectMapper.writeValueAsString(original))))
+                .andExpect(status().isOk())
+                // TODO - Verify the updated object was returned eg;
+                .andExpect(jsonPath("$.name", is(unchangedValue)))
+                .andExpect(jsonPath("$.age", is(2)))
+                .andDo(document(
+                        "{class-name}/{method-name}",
+                        preprocessResponse(prettyPrint()),
+                        /* TODO - If necessary, describe JSON request fields. eg;
+                         * requestFields(fieldWithPath("mandatoryProperty").description("The Person mandatory mandatoryProperty value.")
+                         *  .attributes(key("constraints").value("Must not be null. Must not be empty"))
+                         * requestFields((fieldWithPath("optionalProperty").description("The Person optional optionalProperty value.").optional()
+                         */
+                        requestFields(
+                                fieldWithPath("name").description("The <%= noun %>'s name")
+                                        .attributes(key("constraints").value("Must not be null. Must not be empty")),
+                                fieldWithPath("age").description("The <%= noun %>'s age"),
+                                fieldWithPath("email").description("The <%= noun %> email address")
+                        )));
+
+        verify(<%= nounLowercase %>Service, atLeastOnce()).update(original);
     }
 
     private String prettyPrintRequest(String original) throws IOException {
